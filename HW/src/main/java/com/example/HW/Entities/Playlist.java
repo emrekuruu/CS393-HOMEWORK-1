ed0e68f23c8c93b7c3ba661d5052ae11912ba7d4
emrekuruu <emre.kuru@ozu.edu.tr>
2022-11-29 13:34:29 +0300

    A COUPLE CHANGES AGAIN

--- HW/src/main/java/com/example/HW/Entities/Playlist.java
+++ HW/src/main/java/com/example/HW/Entities/Playlist.java
@@ -1,4 +1,67 @@
 package com.example.HW.Entities;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "T_PLAYLIST")
 public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column
+    String name;
+
+    @ManyToMany
+    List<Song> songs;
+
+    @ManyToMany
+    List<Playlist> playlists;
+
+    public Playlist() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    public List<Playlist> getPlaylists() {
+        return playlists;
+    }
+
+    public void setPlaylists(List<Playlist> playlists) {
+        this.playlists = playlists;
+    }
+
+    @Override
+    public String toString() {
+        return "Playlist{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    
 }
